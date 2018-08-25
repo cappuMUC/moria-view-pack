689ecd277e328a8b02d2f1bcc92c0eae43278f68
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-08-25 08:46:33 +0200

    added initial files

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :moria_view_pack do
+  name 'Moria View Pack'
+  author 'Bernhard Rohloff'
+  description 'This plugin replaces some basic views of Redmine. It's incompatible with the default themes of Redmine!'
+  version '0.0.1'
+  url 'http://github.com/cappumuc/moria-view-pack'
+  author_url 'http://withoutacast.de'
+end
